47947af0f1ce944d39438c4cb4ebb0b019f975fb
youssefelsheikh506 <youssefelsheikh506@gmail.com>
2024-10-11 05:17:19 +0300

    Completed The first Function

--- Ploting.py
+++ Ploting.py
@@ -8,7 +8,6 @@ def Build_Plot(lines):
     y = []
     for points in lines:
         point = points.split(' ')
-        #  print(point)
         x.append(float(point[0]))
         y.append(float(point[1]))
 
@@ -23,10 +22,23 @@ def Build_Plot(lines):
     x_cubic = np.linspace(x.min(), x.max(), 500)
     y_cubic = cubic_interpolation_model(x_cubic)
 
-    plt.plot(x_cubic,y_cubic)
-    plt.style.use('fivethirtyeight')
-    plt.xlabel('Time')
-    plt.ylabel('A')
-    plt.title('Time Domain')
-    plt.legend()
+    frame,(Continuous,Discrete) = plt.subplots(1,2,figsize=(10,4))
+
+    Continuous.plot(x_quad,y_quad)
+    Continuous.axhline(0, color='black',linewidth=1)
+    Continuous.axvline(0, color='black',linewidth=1) 
+    Continuous.grid(True)
+    Continuous.set_xlabel('Time')
+    Continuous.set_ylabel('Amplitude')
+    Continuous.set_title('Analog Signal')
+
+    # Remove The Red Line on The X-axis (basefmt)
+    Discrete.stem(x, y,basefmt='none',linefmt='k-',markerfmt="C0o")
+    Discrete.axhline(0, color='black', linewidth=1)
+    Discrete.axvline(0, color='black', linewidth=1)
+    Discrete.grid(True)
+    Discrete.set_xlabel('Time')
+    Discrete.set_ylabel('Amplitude')
+    Discrete.set_title('Digital Signal')
+
     plt.show()
